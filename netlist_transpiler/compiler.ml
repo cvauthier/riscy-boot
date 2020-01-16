7e68a829c9ac0a1eff8257bbb165c00f77f7e4a5
Christophe Vauthier <cvauthier@ens.fr>
2020-01-17 00:20:01 +0100

    Ajout de BEQ,BNE,BLT,BGE. Correction de quelques bugs dans le transpileur

--- netlist_transpiler/compiler.ml
+++ netlist_transpiler/compiler.ml
@@ -15,10 +15,6 @@ let length n = (n + 7) / 8
 let size_arg env = function | Avar id -> (fst (Hashtbl.find env.vars id))
 														| Aconst (VBit _) -> 1
 														| Aconst (VBitArray v) -> (Array.length v)
-
-let print_var_ident fmt (id,(_,vf)) = 
-	fprintf fmt "%s%s" (match vf with | Local -> "v_" | Input -> "in_" | Output -> "out_") id
-
 let var_to_int var =
 	let res = ref 0 in
 	for i = 0 to (Array.length var)-1 do
@@ -32,90 +28,99 @@ let var_to_val var =
 		res := (2 * !res) + (if var.(i) then 1 else 0)
 	done; !res
 
-let byte_of_var i fmt (id,(size,vf)) =
+
+(* Fonctions pour afficher des variables/arguments *)
+
+let print_var_ident fmt (id,(_,vf)) = 
+	fprintf fmt "%s%s" (match vf with | Local -> "v_" | Input -> "in_" | Output -> "out_") id
+
+let print_var_byte i fmt (id,(size,vf)) =
 	let v = id,(size,vf) in
 	let l = length size in
-	if (vf = Output) && (l = 1) then fprintf fmt "*%a" print_var_ident v
+	if (vf = Output) && (l = 1) then fprintf fmt "(*%a)" print_var_ident v
 	else if l > 1 then fprintf fmt "%a[%d]" print_var_ident v i
 	else fprintf fmt "%a" print_var_ident v
 
-let print_var_decl fmt (id,(size,vf)) = byte_of_var (length size) fmt (id,(size,vf)) 
+let print_var_decl fmt (id,(size,vf)) = print_var_byte (length size) fmt (id,(size,vf)) 
 
-let byte_of_arg env i fmt = function
-	| Avar id -> byte_of_var i fmt (id,Hashtbl.find env.vars id)
+let print_mask fmt l = if l < 8 then fprintf fmt "&%d" ((1 lsl l) - 1)
+
+let print_arg_byte env i fmt = function
+	| Avar id -> print_var_byte i fmt (id,Hashtbl.find env.vars id)
 	| Aconst (VBit b) -> fprintf fmt "%d" (if b then 1 else 0)
 	| Aconst (VBitArray v) -> fprintf fmt "%d" (var_to_val (Array.sub v (i*8) 8)) 
 
-let slice_of_arg env i1 i2 fmt a =
+let print_arg_slice env i1 i2 fmt a =
 	let q = i1 / 8 and r = i1 mod 8 and l = i2-i1+1 in
-	let print_mask fmt l = if l < 8 then fprintf fmt "&%d" ((1 lsl l) - 1) in
 	if r = 0 then
-		fprintf fmt "%a%a" (byte_of_arg env q) a print_mask l
+		fprintf fmt "%a%a" (print_arg_byte env q) a print_mask l
 	else if 8-r >= l then
-		fprintf fmt "(%a>>%d)%a" (byte_of_arg env q) a r print_mask l
+		fprintf fmt "(%a>>%d)%a" (print_arg_byte env q) a r print_mask l
 	else
-		fprintf fmt "((%a>>%d)|(%a<<%d))%a" (byte_of_arg env q) a r
-																			 	 (byte_of_arg env (q+1)) a (8-r) print_mask l
+		fprintf fmt "((%a>>%d)|(%a<<%d))%a" (print_arg_byte env q) a r
+																			 	 (print_arg_byte env (q+1)) a (8-r) print_mask l
 
 let int_of_arg env fmt = function
 	| Avar id -> begin
 			let (size,vf) = Hashtbl.find env.vars id in
 			let l = length size in 
-			let aux fmt i = byte_of_var i fmt (id,(size,vf)) in
-			fprintf fmt "((int) %a)" aux 0;
-			if l >= 2 then fprintf fmt "+((int)%a)<<8" aux 1;
-			if l >= 3 then fprintf fmt "+((int)%a)<<16" aux 2;
-			if l >= 4 then fprintf fmt "+((int)%a)<<24" aux 3
+			let aux fmt i = print_var_byte i fmt (id,(size,vf)) in
+			aux fmt 0;
+			if l >= 2 then fprintf fmt "+(%a<<8)" aux 1;
+			if l >= 3 then fprintf fmt "+(%a<<16)" aux 2;
+			if l >= 4 then fprintf fmt "+(%a<<24)" aux 3
 		end
 	| Aconst (VBit b) -> fprintf fmt "%d" (if b then 1 else 0)
 	| Aconst (VBitArray v) -> fprintf fmt "%d" (var_to_int v)
 
 (* Compile le ième octet d'une expression *)
 let compile_expr_byte env resvar i fmt = function
-	| Earg a -> byte_of_arg env i fmt a
+	| Earg a -> print_arg_byte env i fmt a
 	| Ereg id -> fprintf fmt "st->regs[%d]" (Hashtbl.find env.regs id)  
-  | Enot a -> fprintf fmt "~%a" (byte_of_arg env i) a
+  | Enot a -> fprintf fmt "(~%a)%a" (print_arg_byte env i) a print_mask ((size_arg env a) - 8*i)
   | Ebinop(op,a1,a2) -> 
-		fprintf fmt "%s(%a%s%a)" (match op with | Or | And | Xor -> "" | Nand -> "~")
-								 (byte_of_arg env i) a1
+		fprintf fmt "(%s(%a%s%a))%a" (match op with | Or | And | Xor -> "" | Nand -> "~")
+								 (print_arg_byte env i) a1
 								 (match op with | Or -> "|" | And | Nand -> "&" | Xor -> "^")
-								 (byte_of_arg env i) a2
-  | Emux(a,a1,a2) -> fprintf fmt "%a ? %a : %a" (byte_of_arg env 0) a
-												  (byte_of_arg env i) a1
-												  (byte_of_arg env i) a2
+								 (print_arg_byte env i) a2
+								 print_mask ((size_arg env a1) - 8*i)
+  | Emux(a,a1,a2) -> fprintf fmt "(%a) ? (%a) : (%a)" (print_arg_byte env 0) a
+												  (print_arg_byte env i) a1
+												  (print_arg_byte env i) a2
   | Erom(_,_,raddr) 
   | Eram(_,_,raddr,_,_,_) ->
 			fprintf fmt "st->mem_%s[%a][%d]" resvar (int_of_arg env) raddr i
   | Econcat(a1,a2) -> begin
 			let s1 = size_arg env a1 and s2 = size_arg env a2 in
 			if (i+1)*8 > s1 && i*8 < s1 then 
-				fprintf fmt "(%a)|((%a)<<%d)" (slice_of_arg env (i*8) (s1-1)) a1 (slice_of_arg env 0 ((i+1)*8-s1-1)) a2 (s1 mod 8)
-			else if i*8 < s1 then byte_of_arg env i fmt a1
-			else slice_of_arg env (i*8-s1) (min (i*8-s1+8) (s2-1)) fmt a2
+				fprintf fmt "(%a)|((%a)<<%d)" (print_arg_slice env (i*8) (s1-1)) a1 
+																			(print_arg_slice env 0 (min (s2-1) ((i+1)*8-s1-1))) a2 (s1 mod 8)
+			else if i*8 < s1 then print_arg_byte env i fmt a1
+			else print_arg_slice env (i*8-s1) (min (i*8-s1+8) (s2-1)) fmt a2
 		end
-	| Eselect(i1,a) -> slice_of_arg env i1 i1 fmt a
-  | Eslice(i1,i2,a) -> slice_of_arg env (i1+i*8) (min (i1 + i*8 + 7) i2) fmt a  
+	| Eselect(i1,a) -> print_arg_slice env i1 i1 fmt a
+  | Eslice(i1,i2,a) -> print_arg_slice env (i1+i*8) (min (i1 + i*8 + 7) i2) fmt a  
 
 let compile_eq fmt env (id,expr) = 
 	let size,vf = Hashtbl.find env.vars id in
 	let l = length size in
 	for i = 0 to l-1 do
-		fprintf fmt "@,%a = %a;" (byte_of_var i) (id,(size,vf)) (compile_expr_byte env id i) expr
+		fprintf fmt "@,%a = %a;" (print_var_byte i) (id,(size,vf)) (compile_expr_byte env id i) expr
 	done
 
 let compile_updates fmt env eqs =
 	let aux (id,expr) = match expr with
 		| Eram(_,wsize,_,we,waddr,data) -> begin
 				let l = (wsize+7) / 8 in
-				fprintf fmt "@,if (%a)@,@[<v 2>{" (byte_of_arg env 0) we;
+				fprintf fmt "@,if (%a)@,@[<v 2>{" (print_arg_byte env 0) we;
 				for i = 0 to l-1 do
-					fprintf fmt "@,st->mem_%s[%a][%d] = %a;" id (int_of_arg env) waddr i (byte_of_arg env i) data
+					fprintf fmt "@,st->mem_%s[%a][%d] = %a;" id (int_of_arg env) waddr i (print_arg_byte env i) data
 				done;
 				fprintf fmt "@]@,}"
 			end
 		| Earg _ | Ereg _ | Enot _ | Ebinop _ | Emux _ | Erom _ | Econcat _ | Eselect _ | Eslice _ -> () in
 
-	Hashtbl.iter (fun id i -> fprintf fmt "@,st->regs[%d] = %a;" i (byte_of_var 0) (id,(Hashtbl.find env.vars id))) env.regs; 
+	Hashtbl.iter (fun id i -> fprintf fmt "@,st->regs[%d] = %a;" i (print_var_byte 0) (id,(Hashtbl.find env.vars id))) env.regs; 
 	List.iter aux eqs
 
 let build_env prog =
--- processeur/program.s
+++ processeur/program.s
@@ -1,21 +1,35 @@
 lui 0,%r1
-addi 42,%r1,%r2
-andi 17,%r2,%r3 ; résultat : 0
-xori 9,%r2,%r4 ; résultat : 35
-ori 9,%r2,%r5 ; résultat : 43
-add %r4,%r5,%r6 ; résultat : 78
-sub %r5,%r4,%r7 ; résultat : r5 - r4 = 8
-jal skipped,%r1
-
-add3:
-xor %r11,%r11,%r11
-lw 0,%r11,%r16
-jalr 0,%r1,%r1
+addi 42,%r1,%r2 ; r2 = 42
+andi 17,%r2,%r3 ; r3 =  0
+xori 9,%r2,%r4  ; r4 = 35
+ori 9,%r2,%r5   ; r5 = 43
+add %r4,%r5,%r6 ; r6 = 78
+sub %r5,%r4,%r7 ; r7 = r5 - r4 = 8
+jal lbl,%r1
 
 skipped:
+xor %r11,%r11,%r11
+lw 0,%r11,%r16 ; r16 = 42
+jalr 0,%r1,%r1
+
+lbl:
 xor %r10,%r10,%r10
 xor %r1,%r1,%r1
-addi 42,%r10,%r10
+addi 42,%r10,%r10 ; r10 = 42
 sw %r10,0,%r1
-jal add3,%r1
+jal skipped,%r1
+lui 0,%r13
+
+bne %r2,%r3,sub19
+
+add3:
+addi 3,%r16,%r17 ; r17 = 45
+jal end,%r1
+add5:
+addi 5,%r16,%r17 ; r17 = 47
+jal end,%r1
+sub19:
+addi -19,%r16,%r17 ; r17 = 23
+
+end:
 lui 0,%r13
